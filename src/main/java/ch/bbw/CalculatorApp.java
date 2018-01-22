f56890a0e94b9ab83330b1a3477402c58a543640
5ia15nibertschinger <5ia15nibertschinger@gibwintra.ch>
2018-01-22 08:56:39 +0100

    implementation

--- src/main/java/ch/bbw/CalculatorApp.java
+++ src/main/java/ch/bbw/CalculatorApp.java
@@ -1,11 +1,26 @@
 package ch.bbw;
 
+/**
+ * Console Calculator App
+ * @author Nik Bertschinger
+ * @version 0.0.1
+ * */
+
 public class CalculatorApp {
 
 	public static void main(String[] args) {
 		Calculator calc = new Calculator();
+		int valueA = 0;
+		int valueB = 0;
 		
-		System.out.println(calc.addition(2, 5));
+		System.out.println("Console Calculator");
+		System.out.println("------------------");
+		System.out.println();
+		
+		valueA = 10;
+		valueB = 20;
+		
+		System.out.println("Summe " + valueA + " + " + valueB + " = " + calc.summe(valueA, valueB));
 
 	}
 
